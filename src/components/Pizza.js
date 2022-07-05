056e9d71e750fd77f06f69df27e1a4dfeab671da
pawelpolak88 <nikita.taranaw@gmail.com>
2022-07-05 23:21:50 +0400

    lesson 3

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,4 +1,10 @@
-const Pizza = (props) => {
+import { useState } from 'react'
+
+const Pizza = ({ title, price }) => {
+    const [count, setCount] = useState(0)
+
+    const toggleCount = () => setCount(count + 1)
+
     return (
         <div className="pizza-block">
             <img
@@ -6,7 +12,7 @@ const Pizza = (props) => {
                 src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
                 alt="Pizza"
             />
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
                     <li className="active">тонкое</li>
@@ -19,8 +25,8 @@ const Pizza = (props) => {
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -33,9 +39,9 @@ const Pizza = (props) => {
                             fill="white"
                         />
                     </svg>
-                    <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <span onClick={toggleCount}>Добавить</span>
+                    <i>{count}</i>
+                </button>
             </div>
         </div>
     )
