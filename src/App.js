9f5c39c9bf3304b7f49a71e69670defe5be2442b
pawelpolak88 <nikita.taranaw@gmail.com>
2022-07-07 21:38:44 +0400

    lesson 7

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
+import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header";
-import Category from "./components/Category";
-import Sort from "./components/Sort";
-import Pizza from "./components/Pizza";
-import axios from "axios"
-import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart"
 
 function App() {
-  const [items, setItems] = useState([])
-
-  useEffect(() => {
-    axios.get('https://62c6d70b74e1381c0a6a40ec.mockapi.io/items')
-      .then((response) => response.data)
-      .then((data) => setItems(data)
-      )
-  }, [])
-
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Category />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((item, key) =>
-              <Pizza key={item.id}
-                imageUrl={item.imageUrl}
-                title={item.title}
-                price={item.price}
-                types={item.types}
-                sizes={item.sizes}
-                category={item.category}
-                rating={item.rating}
-              />
-            )}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import Category from "../components/Category";
+import Sort from "../components/Sort";
+import Pizza from "../components/Pizza";
+import PizzaSkeleton from "../components/PizzaSkeleton";
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
+const Home = () => {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        axios.get('https://62c6d70b74e1381c0a6a40ec.mockapi.io/items')
+            .then((response) => response.data)
+            .then((data) => {
+                setItems(data)
+                setIsLoading(false)
+            })
+    }, [])
+
+    return (
+        <>
+            <div className="content__top">
+                <Category />
+                <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading ?
+                    [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
+                    :
+                    items.map((item, key) =>
+                        <Pizza key={key}
+                            imageUrl={item.imageUrl}
+                            title={item.title}
+                            price={item.price}
+                            types={item.types}
+                            sizes={item.sizes}
+                            category={item.category}
+                            rating={item.rating}
+                        />
+                    )}
+            </div>
+        </>
+    );
+}
+
+export default Home
